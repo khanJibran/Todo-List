e8a69e275914edba1716195ec584f212d7fe421a
khanjibran <khanjibran38@yahoo.com>
2019-11-18 15:37:20 +0100

    Todo list

---
+++ todos/src/Component/ToDo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
+const ToDo = props => {
+  return props.todos.map(item => (
+    <TodoItem
+      key={item.id}
+      myItem={item}
+      toggleComplete={props.toggleComplete}
+      deletItem={props.deletItem}
+    />
+  ));
+};
+// we can not pass state through components
+​
+ToDo.propTypes = {
+  todos: PropTypes.array.isRequired,
+  toggleComplete: PropTypes.func.isRequired
+};
+​
+export default ToDo;
